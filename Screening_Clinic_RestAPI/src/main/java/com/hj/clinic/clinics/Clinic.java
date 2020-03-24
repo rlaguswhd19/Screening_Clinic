45f50937c93dab2844f5cba312824ba31dbcff2d
rlaguswhd19 <rlaguswhd19@naver.com>
2020-03-24 12:10:25 +0900

    csvFile insert into Mysql

--- Screening_Clinic_RestAPI/src/main/java/com/hj/clinic/clinics/Clinic.java
+++ Screening_Clinic_RestAPI/src/main/java/com/hj/clinic/clinics/Clinic.java
@@ -6,7 +6,6 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -14,23 +13,34 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Builder @AllArgsConstructor @NoArgsConstructor
-@Setter @Getter @EqualsAndHashCode(of = "id")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode(of = "id")
 @Entity
 public class Clinic {
-	@Id @GeneratedValue
+	@Id
+	@GeneratedValue
 	private Integer id;
-	
+
 	@Enumerated(EnumType.STRING)
 	private ClinicCollection clinicCollection = ClinicCollection.N;
-	
+
 	private String city;
-	
+
 	private String town;
-	
+
 	private String clinicName;
-	
+
 	private String address;
-	
+
 	private String calling;
+
+	@Override
+	public String toString() {
+		return "Clinic [id=" + id + ", clinicCollection=" + clinicCollection + ", city=" + city + ", town=" + town
+				+ ", clinicName=" + clinicName + ", address=" + address + ", calling=" + calling + "]";
+	}
 }
--- Screening_Clinic_RestAPI/src/main/java/com/hj/clinic/config/AppConfig.java
+++ Screening_Clinic_RestAPI/src/main/java/com/hj/clinic/config/AppConfig.java
@@ -6,41 +6,66 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import javax.lang.model.element.NestingKind;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.hj.clinic.clinics.Clinic;
+import com.hj.clinic.clinics.ClinicCollection;
+import com.hj.clinic.clinics.ClinicRepository;
 import com.hj.clinic.common.AppProperties;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
+import com.sun.jdi.connect.spi.Connection;
 
 @Configuration
 public class AppConfig {
-	
+
 	@Autowired
 	AppProperties appProperties;
+
+	@Autowired
+	ClinicRepository clinicRepository;
 	
 	@Bean
 	public ApplicationRunner applicationRunner() {
 		return new ApplicationRunner() {
-			
+
 			@Override
 			public void run(ApplicationArguments args) throws Exception {
+				
 				dataInsert();
 			}
 		};
 	}
 	
-	public ArrayList<String> dataInsert() throws CsvValidationException, IOException {
-			CSVReader csvReader = new CSVReader(new InputStreamReader(new FileInputStream(appProperties.getCsvPath()),"EUC-KR"));
-			
-			String[] nextRecord;
-			while((nextRecord = csvReader.readNext()) != null) {
-				System.out.println(Arrays.toString(nextRecord));
+	public void dataInsert() throws CsvValidationException, IOException {
+		CSVReader csvReader = new CSVReader(
+				new InputStreamReader(new FileInputStream(appProperties.getCsvPath()), "EUC-KR"));
+
+		String[] nextRecord;
+		int index = 0;
+		while ((nextRecord = csvReader.readNext()) != null) {
+//			System.out.println(Arrays.toString(nextRecord));
+			if(index == 0) {
+				index++;
+				continue;
 			}
-		
-		return null;
+			
+			Clinic clinic = Clinic.builder()
+					.clinicCollection(ClinicCollection.valueOf(nextRecord[1]))
+					.city(nextRecord[2])
+					.town(nextRecord[3])
+					.clinicName(nextRecord[4])
+					.address(nextRecord[5])
+					.calling(nextRecord[6])
+					.build();
+//			
+			clinicRepository.save(clinic);
+		}
 	}
 }
