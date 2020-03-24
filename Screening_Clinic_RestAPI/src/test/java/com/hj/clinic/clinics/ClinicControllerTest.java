196fe307cf333932454bbb8759158b11e2f710b4
rlaguswhd19 <rlaguswhd19@naver.com>
2020-03-24 19:34:43 +0900

    RestDocs snippets 구현 RestDocsConfiguration prettyprint

--- Screening_Clinic_RestAPI/src/test/java/com/hj/clinic/clinics/ClinicControllerTest.java
+++ Screening_Clinic_RestAPI/src/test/java/com/hj/clinic/clinics/ClinicControllerTest.java
@@ -1,5 +1,6 @@
-package com.hj.clinic.Clinics;
+package com.hj.clinic.clinics;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -10,17 +11,23 @@ import javax.transaction.Transactional;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.hj.clinic.common.RestDocsConfiguration;
+
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@Import(RestDocsConfiguration.class)
 @ActiveProfiles("test")
 public class ClinicControllerTest {
 	
@@ -42,7 +49,8 @@ public class ClinicControllerTest {
 		.andDo(print())
 		.andExpect(status().isOk())
 		.andExpect(jsonPath("page").exists())
-		.andExpect(jsonPath("profile").exists())
+//		.andExpect(jsonPath("profile").exists())
+		.andDo(document("lists-clinic"))
 		;
 	}
 }
